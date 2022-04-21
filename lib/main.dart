580978f0a602f9ecbe4721d611a488d8f225bb04
Soumya Bhatnagar <soumya11207@gmail.com>
2022-04-21 04:22:39 -0700

    build widget tree

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: Text("default body text here"),
-      ),
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Text('Question text?'),
+              ElevatedButton(onPressed: null, child: Text('answer 1')),
+              ElevatedButton(onPressed: null, child: Text('answer 2')),
+              ElevatedButton(onPressed: null, child: Text('answer 3')),
+            ],
+          )),
     );
   }
 }
