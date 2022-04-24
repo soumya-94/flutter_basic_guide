52e4a2ebb0ef5c73553f7a779b41917fd68e8c01
Soumya Bhatnagar <soumya11207@gmail.com>
2022-04-24 15:05:50 -0700

    calculated total score, used getter

--- lib/result.dart
+++ lib/result.dart
@@ -5,12 +5,27 @@ class Result extends StatelessWidget {
 
   Result(this.resultantScore);
 
+  String get resultPhrase {
+    String text;
+    if (resultantScore <= 8)
+      text = 'innocent';
+    else if (resultantScore <= 12)
+      text = 'nice';
+    else if (resultantScore <= 16)
+      text = 'ok';
+    else
+      text = 'bad!';
+
+    return text;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Text(
-        "Completed!",
+        resultPhrase,
         style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
       ),
     );
   }
